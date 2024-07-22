67da3c3518b16e5ec18f467cf2b940e651554fcc
Rishabh Jain <106752253+jainrishabh23@users.noreply.github.com>
2024-07-22 14:28:33 +0530

    Time: 37 ms (43.52%), Space: 16.4 MB (51.63%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -3,11 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Approach:
+#  Using hare and tortoise method for finding middle node in LL
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = head
-        fast = head
+        # Storing head into temp variable
+        temp = head
+        # assigning slow pointer at temp(head)
+        slow = temp
+        # assigning fast pointer at temp(head)
+        fast = temp
+        # Checking for the end node i.e. null 
         while(fast and fast.next):
             slow = slow.next
             fast = fast.next.next
+        # returning the slow pointer or returning second half of linked list
         return slow
