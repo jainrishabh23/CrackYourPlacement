d9f89dbe6da60443cf71b81aa1db3f70882e2b73
Rishabh Jain <106752253+jainrishabh23@users.noreply.github.com>
2024-07-22 22:07:25 +0530

    Time: 41 ms (50.86%), Space: 16.5 MB (77.79%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -3,16 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Approach:
+# Linear check of node and next of node
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # node is there or not
         if not head:
-            return 
+            return
+        # assigning temp to head       
         temp = head
-        # temp2 = head.next
+        # Check 
         while(temp and temp.next):
+            # checking node and next node value
             if temp.val == temp.next.val:
+                # Skipping out duplicate node
                 temp.next = temp.next.next
             else:
                 temp = temp.next
         return head
-        
+
+# Time Complexity --    O(N)  as it traverse one time
+# Space Complexity --   O(1)  as it not use any extra space 
