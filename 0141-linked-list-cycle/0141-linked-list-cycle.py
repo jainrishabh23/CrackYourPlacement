2dc09f57c9194dcf266493964736d6ee7b26570e
Rishabh Jain <106752253+jainrishabh23@users.noreply.github.com>
2024-07-22 16:33:06 +0530

    Time: 39 ms (89.51%), Space: 19.1 MB (49%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Approach:
+#  Using hare and tortoise method for finding cycle in LL
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Storing head to temp variable
+        temp = head
+        # assigning slow pointer to temp(head)
+        slow = temp
+        # assigning fast pointer to temp(head)
+        fast = temp
+        # Checking for the end node i.e. null 
+        while(fast and fast.next):
+            # move slow pointer by one step
+            slow = slow.next
+            # move fast pointer by two step
+            fast = fast.next.next
+            # Checking slow and fast pointer collision
+            if slow == fast:
+                # returning true in case of collision or loop
+                return True
