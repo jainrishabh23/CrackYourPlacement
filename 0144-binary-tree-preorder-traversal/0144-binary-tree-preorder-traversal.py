1075c3aefa4017f9d0e0f1ff7bef2cbb2556c7c7
Rishabh Jain <106752253+jainrishabh23@users.noreply.github.com>
2024-07-22 22:10:42 +0530

    Time: 39 ms (27.46%), Space: 16.4 MB (48.78%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -4,19 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+#     /  -1
+#    /  /   \
+#   | -2    |-3     
+#   |_______|
+
+# Root -> Left -> Right
+
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # initializing array
         arr = []
+        # defining helper function for preorder
         def preorder(root):
+            # check of root available or not
             if not root :
                 return
+            # appending value to array visit of current node
             arr.append(root.val)
+            # Traverse the left subtree
             preorder(root.left)
+            # Traverse the right subtree
             preorder(root.right)
+        # calling func for traversal
         preorder(root)
+        # returning array
         return arr
 
 
-
-
-        
+# Time Complexity --    O(N)  as it traverse one time
+# Space Complexity --   O(N)  as it use n extra space
